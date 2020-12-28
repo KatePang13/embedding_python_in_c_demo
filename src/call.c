23cd5c269cad6e939cb9e569a319ce9d5462157d
刘炎宏 <liuyanhong@bonree.com>
2020-12-28 17:56:31 +0800

    Update call.c
    export python module from clang

--- src/call.c
+++ src/call.c
@@ -1,6 +1,36 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+static int numargs=0;
+
+/* Return the number of arguments of the application command line */
+static PyObject*
+emb_numargs(PyObject *self, PyObject *args)
+{
+    if(!PyArg_ParseTuple(args, ":numargs"))
+        return NULL;
+    return PyLong_FromLong(numargs);
+}
+
+//声明 module emb 方法列表
+static PyMethodDef EmbMethods[] = {
+    {"numargs", emb_numargs, METH_VARARGS,
+     "Return the number of arguments received by the process."},
+    {NULL, NULL, 0, NULL}
+};
+
+//声明 module: emb 
+static PyModuleDef EmbModule = {
+    PyModuleDef_HEAD_INIT, "emb", NULL, -1, EmbMethods,
+    NULL, NULL, NULL, NULL
+};
+
+static PyObject*
+PyInit_emb(void)
+{
+    return PyModule_Create(&EmbModule);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -12,6 +42,9 @@ main(int argc, char *argv[])
         fprintf(stderr,"Usage: call pythonfile funcname [args]\n");
         return 1;
     }
+    
+    numargs = argc;
+	PyImport_AppendInittab("emb", &PyInit_emb);//使 emb.numargs 在嵌套的python解释器中可用
 
     Py_Initialize();
     pName = PyUnicode_DecodeFSDefault(argv[1]);
